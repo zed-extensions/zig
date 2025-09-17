b7eb4625b8cad6fc25ce4b956696954180222701
Jakub Konka <kubkon@jakubkonka.com>
2025-09-17 13:13:24 +0200

    Tasks and runnables (#11)
    Extracted from #10 
    
    Implements custom tasks:
    * `zig build test`
    * `zig build run`
    * `zig test $ZED_FILE --test-filter $ZED_CUSTOM_ZIG_TEST_NAME` - which
    runs during runnable code detection

---
+++ languages/zig/runnables.scm
@@ -0,0 +1,16 @@
+; Tag unit tests
+(
+    (test_declaration
+        (string) @name @ZIG_TEST_NAME
+    ) @run
+    (#set! tag zig-test)
+)
+
+; Tag main
+(
+    (function_declaration
+        name: (identifier) @_name
+    ) @run
+    (#match? @_name "main")
+    (#set! tag zig-build-run)
+)
